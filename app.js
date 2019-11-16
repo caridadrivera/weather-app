d212c3f177e8484993af4dcb412fabe1cc38f960
caridadrivera <ccrive10@gmail.com>
2019-11-16 09:46:38 -0500

    save API

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ window.addEventListener('load', ()=> {
     let lat;
 
     if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
 
-    }else {
-        
+            const api = `https://api.darksky.net/forecast/f00c94ee3dba375b6fbf4f29752e096f/${lat},${long}`;
+
+            fetch(api)
+            .then(res => {
+                return res.json();
+            })
+    });//end of if statement
+
     }
-});
+
+    
+});//end of window event listener
